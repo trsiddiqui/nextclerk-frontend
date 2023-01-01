e8ce622a0cb45a0dd09921e0fd9714027c28155a
Taha Siddiqui <tsiddiqui@touchbistro.com>
2023-01-01 12:06:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+```bash
+# For npm
+npm install --legacy-peer-deps
+
+# For yarn
+yarn install
+```
+
+```bash
+# For npm
+npm run dev
+
+# For yarn
+yarn dev
+```
---
+++ src/@core/layouts/components/vertical/navigation/VerticalNavHeader.tsx
@@ -0,0 +1,55 @@
+// ** React Import
+import { ReactNode } from 'react'
+
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Imports
+import Box, { BoxProps } from '@mui/material/Box'
+import { styled } from '@mui/material/styles'
+
+// ** Type Import
+import { Settings } from 'src/@core/context/settingsContext'
+
+interface Props {
+  hidden: boolean
+  settings: Settings
+  toggleNavVisibility: () => void
+  saveSettings: (values: Settings) => void
+  verticalNavMenuBranding?: (props?: any) => ReactNode
+}
+
+// ** Styled Components
+const MenuHeaderWrapper = styled(Box)<BoxProps>(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingRight: theme.spacing(4.5),
+  transition: 'padding .25s ease-in-out',
+  minHeight: theme.mixins.toolbar.minHeight
+}))
+
+const StyledLink = styled('a')({
+  display: 'flex',
+  alignItems: 'center',
+  textDecoration: 'none'
+})
+
+const VerticalNavHeader = (props: Props) => {
+  // ** Props
+  const { verticalNavMenuBranding: userVerticalNavMenuBranding } = props
+
+  return (
+    <MenuHeaderWrapper className='nav-header' sx={{ pl: 6 }}>
+      {userVerticalNavMenuBranding ? (
+        userVerticalNavMenuBranding(props)
+      ) : (
+        <Link href='/' passHref>
+          <StyledLink>NextClerk Logo</StyledLink>
+        </Link>
+      )}
+    </MenuHeaderWrapper>
+  )
+}
+
+export default VerticalNavHeader
