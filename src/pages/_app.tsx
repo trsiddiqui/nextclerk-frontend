b6293ffa59c492b19641a87bf398603fe049d386
Taha Siddiqui <tsiddiqui@touchbistro.com>
2023-03-25 20:03:59 -0500

    added syncfusion spreadsheet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,6 +29,7 @@ import 'react-perfect-scrollbar/dist/css/styles.css'
 
 // ** Global css styles
 import '../../styles/globals.css'
+import { registerLicense } from '@syncfusion/ej2-base'
 
 // ** Extend App Props with Emotion
 type ExtendedAppProps = AppProps & {
@@ -53,6 +54,10 @@ if (themeConfig.routingLoader) {
 
 // ** Configure JSS & ClassName
 const App = (props: ExtendedAppProps) => {
+  console.log('license registered')
+  registerLicense(
+    'Mgo+DSMBaFt/QHRqVVhkVFpAaV5EQmFJfFBmRGJTel56d1JWESFaRnZdQV1gSHlSdEZrWHpcdHNW;Mgo+DSMBPh8sVXJ0S0J+XE9AflRGQmJPYVF2R2BJfl96cVVMY1tBNQtUQF1hSn5Qd0JiW3xWcXFXQGJf;ORg4AjUWIQA/Gnt2VVhkQlFacldJWXxIeEx0RWFab19xflBGal9WVAciSV9jS31TdURkWH1deHVQRGdbUg==;MTM2MDQzNUAzMjMwMmUzNDJlMzBLbm54WTdkTlVCMXJZRXZlU1N2S1Y3SU9MNHJaY0V1WWMwOXd0NzNjUVYwPQ==;MTM2MDQzNkAzMjMwMmUzNDJlMzBFcnVpdGtOMURqaVJ3TkREaVg4NkhUL0RqZFV1UmpRV2RCRUY0aVNJNXc0PQ==;NRAiBiAaIQQuGjN/V0Z+WE9EaFtKVmdWf1VpR2NbfE5xdV9DYFZSQ2YuP1ZhSXxQdkZiXn5cc3xUQ2NbUEM=;MTM2MDQzOEAzMjMwMmUzNDJlMzBGWGNrWkNrbzcyaDgwd05Hazd2cVZaZU42K3krdktLdmVBZmNVaGNLTEVFPQ==;MTM2MDQzOUAzMjMwMmUzNDJlMzBRRVJENFV3M0gvK21yVEVSKzBCZ2VzL2czbDZmbURRWjhJc0xRSDZkdHN3PQ==;Mgo+DSMBMAY9C3t2VVhkQlFacldJWXxIeEx0RWFab19xflBGal9WVAciSV9jS31TdURkWH1deHVQQmNcWQ==;MTM2MDQ0MUAzMjMwMmUzNDJlMzBCMTQwZVcwQzJ5d1FOcnMzMWN5MExvUkxkMG9XS01lSjdzY2F5a2FNKzNJPQ==;MTM2MDQ0MkAzMjMwMmUzNDJlMzBUcUhLbWpRYUJ3YjBUUVVBUW13YnBWT2J3M0xvRlVHZ1E5K2VOZmIzVllBPQ==;MTM2MDQ0M0AzMjMwMmUzNDJlMzBGWGNrWkNrbzcyaDgwd05Hazd2cVZaZU42K3krdktLdmVBZmNVaGNLTEVFPQ=='
+  )
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
   // Variables
