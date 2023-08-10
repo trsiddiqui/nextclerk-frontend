f4b1ae855a3cb3e44497aa68ee9e37fcd1f82b48
Taha Siddiqui <tsiddiqui@touchbistro.com>
2023-08-10 12:04:55 -0500

    showing side bar

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -47,7 +47,12 @@ const UserLayout = ({ children }: Props) => {
       hidden={hidden}
       settings={settings}
       saveSettings={saveSettings}
-      verticalNavItems={session?.token ? VerticalNavItems() : []} // Navigation Items
+      verticalNavItems={
+        // session?.token ?
+        VerticalNavItems()
+
+        // : []
+      } // Navigation Items
       verticalAppBarContent={(
         props // AppBar Content
       ) => (
