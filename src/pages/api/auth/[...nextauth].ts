32a59cc494b558eff891dd9ad9641a3dc9f09956
Taha Siddiqui <tsiddiqui@touchbistro.com>
2023-08-29 01:05:26 -0500

    keycloak in EC2

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,7 @@ import KeycloakProvider from 'next-auth/providers/keycloak'
 
 const clientId = 'nextclerk'
 const clientSecret = 'uvmAPUcHKYG9ZZXsymO9QP3rFAPPmLhF'
-const host = process.env.NODE_ENV === 'production' ? 'http://test.nextclerk.com' : 'http://localhost'
+const host = process.env.NODE_ENV === 'production' ? 'http://18.232.142.190' : 'http://localhost'
 console.log('using', host)
 async function refreshAccessToken(token: JWT & Account) {
   try {
